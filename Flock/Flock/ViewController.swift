d7cec4ed9ba48539ae4ed17d4446fd62997b8b02
ahmedhumza94 <ahmedhumza94@gmail.com>
2018-04-22 12:01:48 -0500

    Style: Initial project architecture
    Initialize Swift Xcode project.

---
+++ Flock/Flock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flock
+//
+//  Created by Humza Ahmed on 4/18/18.
+//  Copyright © 2018 Humza Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
